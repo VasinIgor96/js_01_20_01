ee0a2039128cfb5311c2f905c682617891859a8c
Igor Vasin <vasin.igor0796@gmail.com>
2024-01-20 16:31:06 +0200

    first commit

---
+++ js-hw-01.js
@@ -0,0 +1,18 @@
+let salary = prompt("Введіть розмір вашої зарплати");
+salary = parseFloat(salary);
+if (!isNaN(salary)) {
+    salary = parseFloat(salary);
+    // премія
+    let premium = 0.15 * salary;
+    // нарахування
+    let accrual = salary + premium;
+    // податки
+    let taxes = 0.10 * accrual;
+    // витрати
+    let costs = 190;
+    // залишок
+    let remainder = accrual - taxes - costs;
+    alert ("Залишок від зарплати складає:" + remainder);
+} else {
+    alert("Будь ласка, введіть коректне числове значення для зарплати.");
+}
